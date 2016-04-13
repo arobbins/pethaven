531f182a303d97c75a826706e69dbec0f2ce4f9f
arobbins <arobbins@simpleblend.net>
2016-04-13 13:49:07 -0500

    Added custom login image and custom wordpress login URL

--- content/themes/pethaven/lib/customizer.php
+++ content/themes/pethaven/lib/customizer.php
@@ -34,20 +34,3 @@ function add_menuclass($ulclass) {
   return preg_replace('/<a rel="btn btn-secondary"/', '<a rel="btn" class="btn btn-secondary"', $ulclass, 1);
 }
 add_filter('wp_nav_menu', __NAMESPACE__ . '\\add_menuclass');
-
-
-/*
-
-Changing the default Wordpress login logo
-
-*/
-function custom_login_logo() { ?>
-  <style type="text/css">
-    .login h1 a {
-      background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/imgs/logo-primary.svg);
-      width: 320px;
-      background-size: contain;
-    }
-  </style>
-<?php }
-add_action('login_enqueue_scripts', __NAMESPACE__ . '\\custom_login_logo');
