cb53f0832f6771912968a732349d3ad6c1503abd
arobbins <arobbins@simpleblend.net>
2016-04-29 12:53:27 -0500

    Added FB Event link and dates

--- content/themes/pethaven/components/facebook-events/view.php
+++ content/themes/pethaven/components/facebook-events/view.php
@@ -14,12 +14,19 @@
       $event_title = get_the_title();
       $event_desc =  get_the_content();
       $event_image = get_fbe_image('cover');
+      $event_link = get_fbe_field('fb_event_uri');
+      $event_starts = get_fbe_date('event_starts','M j, Y @ g:i a');
+      $event_ends = get_fbe_date('event_ends','M j, Y @ g:i a');
 
 ?>
-  <img src="<?php echo get_fbe_image('cover'); ?>" alt="" />
+<div class="facebook-event">
+  <a href="<?php echo $event_link; ?>">
+    <img src="<?php echo get_fbe_image('cover'); ?>" alt="" />
+  </a>
   <h1><?php echo $event_title; ?></h1>
+  <p>Starts: <?php echo $event_starts; ?> Ends: <?php echo $event_ends; ?></p>
   <p><?php echo $event_desc; ?></p>
-
+</div>
 <?php
     endwhile;
   endif;
