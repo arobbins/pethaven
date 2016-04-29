9bc1189b5f6e97dfc8c2d96a700d31f179891116
arobbins <arobbins@simpleblend.net>
2016-04-29 14:16:30 -0500

    Added check for Team image

--- content/themes/pethaven/components/team/view.php
+++ content/themes/pethaven/components/team/view.php
@@ -8,7 +8,9 @@
 
   <?php while($loop->have_posts()) : $loop->the_post(); ?>
     <div class="grid-item l-col team-member">
-      <img src="<?php the_field('team_main_image', get_the_id()); ?>" class="team-image">
+      <?php if(get_field('team_main_image', get_the_id())) { ?>
+        <img src="<?php the_field('team_main_image', get_the_id()); ?>" class="team-image">
+      <?php } ?>
       <h2 class="team-name"><?php the_field('team_name', get_the_id()); ?></h2>
       <h3 class="team-title"><?php the_field('team_title', get_the_id()); ?></h3>
       <div class="team-bio">
