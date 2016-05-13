e54c6d5d5618a3e653e102c05c64da401c9c0c11
arobbins <arobbins@simpleblend.net>
2016-05-13 14:28:25 -0500

    Added conditionals for showing / hiding marquee content

--- content/themes/pethaven/components/marquees/view.php
+++ content/themes/pethaven/components/marquees/view.php
@@ -18,11 +18,23 @@
         <div class="l-col l-row-center page-header-background">
           <div class="l-contain l-row-center l-box">
             <div class="marquee-content">
-              <h1 class="marquee-heading"><?php the_sub_field('marquee_heading'); ?></h1>
-              <div class="marquee-copy">
-                <?php the_sub_field('marquee_body_copy'); ?>
-              </div>
-              <a href="<?php the_sub_field('marquee_button_link'); ?>" class="btn btn-large marquee-link"><?php the_sub_field('marquee_button_copy'); ?></a>
+
+              <?php if(get_sub_field('marquee_heading')) { ?>
+                <h1 class="marquee-heading">
+                  <?php the_sub_field('marquee_heading'); ?>
+                </h1>
+              <?php } ?>
+
+              <?php if(get_sub_field('marquee_body_copy')) { ?>
+                <div class="marquee-copy">
+                  <?php the_sub_field('marquee_body_copy'); ?>
+                </div>
+              <?php } ?>
+
+              <?php if(get_sub_field('marquee_button_link')) { ?>
+                <a href="<?php the_sub_field('marquee_button_link'); ?>" class="btn btn-large marquee-link"><?php the_sub_field('marquee_button_copy'); ?></a>  
+              <?php } ?>
+
             </div>
           </div>
         </div>
