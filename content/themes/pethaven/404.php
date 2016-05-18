8c15a14a1b7b49f433b7a389183aee928ecc6176
arobbins <arobbins@simpleblend.net>
2016-05-18 11:53:42 -0500

    Removed hello from 404 page

--- content/themes/pethaven/404.php
+++ content/themes/pethaven/404.php
@@ -1,7 +1,5 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
-<h1>Hello</h1>
-
 <div class="alert alert-warning">
   <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
 </div>
